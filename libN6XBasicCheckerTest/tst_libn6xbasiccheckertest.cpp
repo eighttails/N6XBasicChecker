578db0aafb3c1677050ebcf5bf420bdf9f3f9d33
Tadahito Yao <eighttails@nifty.com>
2012-06-10 16:44:58 +0900

    テストケース追加

--- libN6XBasicCheckerTest/tst_libn6xbasiccheckertest.cpp
+++ libN6XBasicCheckerTest/tst_libn6xbasiccheckertest.cpp
@@ -30,20 +30,27 @@ bool LibN6XBasicCheckerTest::parse(const std::wstring& program, ParserStatus& st
 void LibN6XBasicCheckerTest::testCase1()
 {
     ParserStatus stat;
+    std::wstring programList;
     //正常系
-    std::wstring str =
+    programList =
             L"10 goto 10: go to10\n"
+            "\n"
             "20 goto 20\n";
-    QVERIFY(parse(str, stat));
+    QVERIFY(parse(programList, stat));
 
     //エラー行判定
-    str = L"10 goto 10: go to10\n"
-            "20 goo 20\n"
-            "30 goto 30\n";
-    QVERIFY(!parse(str, stat));
-    QVERIFY(stat.errorList_.size() == 1);
+    programList =
+            L"10 goto 10: go to10\n"
+            "20 goo 20\n"       //エラー
+            "\n"
+            "30 goto 30\n"
+            "40 goto-40\n";     //エラー
+    QVERIFY(!parse(programList, stat));
+    QVERIFY(stat.errorList_.size() == 2);
     QVERIFY(stat.errorList_[0].textLineNumber_ ==  2);
     QVERIFY(stat.errorList_[0].basicLineNumber_ == 20);
+    QVERIFY(stat.errorList_[1].textLineNumber_ ==  5);
+    QVERIFY(stat.errorList_[1].basicLineNumber_ == 40);
 }
 
 QTEST_APPLESS_MAIN(LibN6XBasicCheckerTest);
