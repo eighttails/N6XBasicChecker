305b368f5cf067f4b5edb5e000ec280c7d70f06b
Tadahito Yao <eighttails@nifty.com>
2018-11-24 15:01:56 +0900

    ビルド用ディレクトリを変更

--- win32/buildrelease.sh
+++ win32/buildrelease.sh
@@ -11,6 +11,12 @@ export SCRIPT_DIR=$(dirname $(readlink -f ${BASH_SOURCE:-$0}))
 #並列ビルド
 MINGW32MAKE="mingw32-make -j$NUMBER_OF_PROCESSORS"
 
+if [ -z $1 ]; then
+    cd $SCRIPT_DIR/../../
+else
+    cd $1 
+fi
+
 N6XBC_DBUILD_DIR=N6XBasicChecker-build-$MINGW_CHOST
 
 if [ -e $N6XBC_DBUILD_DIR ]; then 
