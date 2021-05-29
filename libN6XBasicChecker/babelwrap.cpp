45a75e4106721b087caf7a2dbe869d36f58a174d
Tadahito Yao <eighttails@nifty.com>
2021-05-29 10:43:01 +0900

    WIP

--- libN6XBasicChecker/babelwrap.cpp
+++ libN6XBasicChecker/babelwrap.cpp
@@ -1,1 +1,33 @@
+#include <iostream>
+#include <locale>
+#include <locale.h>
 #include "babel.cpp"
+
+
+//#define utf8_to_local(a) babel::utf8_to_sjis(a)
+//#define unicode_to_local(a) babel::unicode_to_sjis(a)
+//#define local_to_unicode(a) babel::sjis_to_unicode(a)
+
+const std::string utf8_to_local(const std::string &str)
+{
+	printf("%s\n", setlocale(LC_ALL, NULL));
+//	auto global_locale = std::locale("");
+//	std::locale global_locale("");
+//	std::cout << global_locale.name() << std::endl;
+	if (1){
+		return babel::utf8_to_sjis(str);
+	} else {
+		return babel::utf8_to_sjis(str);
+
+	}
+}
+
+const std::string unicode_to_local(const std::wstring &str)
+{
+	return babel::unicode_to_sjis(str);
+}
+
+const std::wstring local_to_unicode(const std::string &str)
+{
+	return babel::sjis_to_unicode(str);
+}
--- libN6XBasicChecker/babelwrap.h
+++ libN6XBasicChecker/babelwrap.h
@@ -2,8 +2,10 @@
 #define BABELWRAP_H
 
 //babelに関連するヘッダーおよびマクロ
-
 #include "babel.h"
+#include <string>
+
+#if 0
 #ifdef WIN32
 #define utf8_to_local(a) babel::utf8_to_sjis(a)
 #define unicode_to_local(a) babel::unicode_to_sjis(a)
@@ -13,5 +15,10 @@
 #define unicode_to_local(a) babel::unicode_to_utf8(a)
 #define local_to_unicode(a) babel::utf8_to_unicode(a)
 #endif
+#endif // if 0
+
+const std::string utf8_to_local(const std::string& str);
+const std::string unicode_to_local(const std::wstring& str);
+const std::wstring local_to_unicode(const std::string& str);
 
 #endif // BABELWRAP_H
